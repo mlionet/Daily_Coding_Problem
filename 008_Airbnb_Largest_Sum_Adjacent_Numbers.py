de7816c0071631a0c87ae8892976de1f2cc98a19
vduong <vinceduong@hotmail.fr>
2018-12-28 20:02:39 +0100

    008 finished

--- 008_Airbnb_Largest_Sum_Adjacent_Numbers.py
+++ 008_Airbnb_Largest_Sum_Adjacent_Numbers.py
@@ -8,3 +8,18 @@ def largest_sum(array) :
         return 0
     if (len(array) == 1) :
         return array[0]
+    value1 = array[0] + largest_sum(array[2:len(array)])
+    value2 = largest_sum(array[1:len(array)])
+    if (value1 > value2) :
+        return value1
+    return value2
+
+##----TEST ZONE----##
+
+if __name__ == "__main__" : 
+    #INTERVIEW TEST
+    print(largest_sum([2, 4, 6, 2, 5]))#should give 13
+    print(largest_sum([5, 1, 1, 5]))#should give 10
+    print(largest_sum([10, 10, 10]))#should give 20
+    print(largest_sum([1, 1, 0, -1]))#should give 1
+    print(largest_sum([10,0,0,9,10]))#should give 20
